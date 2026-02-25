92f2012d253f2465ef4d0822cceb1b3df241ce3e
fearlnwzaa_eiei <1166105050213@mail.rmutt.ac.th>
2026-02-25 16:45:55 +0700

    Update multiple_classification.py

--- multiple_classification.py
+++ multiple_classification.py
@@ -1,183 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 18 16:03:42 2026
- 
-@author: Lab
-"""
- 
-import pickle
-import streamlit as st
-from streamlit_option_menu import option_menu
- 
-riding_model = pickle.load(open("Riding_model.sav",'rb'))
-loan_model = pickle.load(open("loan_model.sav",'rb'))
-bmi_model = pickle.load(open("bmi_model.sav",'rb'))
- 
- 
-with st.sidebar:
-    selected = option_menu(
-        'Classification',['Loan','Riding','BMI']
-        )
- 
-gender_map = {
-    'Male':1,
-    'Female':0
-    }
- 
-education_map = {
-    'Associate': 0,
-    'Bachelor': 1,
-    'Doctorate': 2,
-    'High School': 3,
-    'Master': 4
-}
- 
-home_map = {
-    'MORTGAGE': 0,
-    'OTHER': 1,
-    'OWN': 2,
-    'RENT': 3
-}
- 
-intent_map = {
-    'DEBTCONSOLIDATION': 0,
-    'EDUCATION': 1,
-    'HOMEIMPROVEMENT': 2,
-    'MEDICAL': 3,
-    'PERSONAL': 4,
-    'VENTURE': 5
-}
- 
-default_map = {
-    'No': 0,
-    'Yes': 1
-}
 if (selected == 'BMI'):
     st.title('BMI Classification')
-    person_gender = st.selectbox('Gender', gender_map)
-    height = st.number_input('Height (cm)', min_value=0.0)
-    weight = st.number_input('Weight (kg)', min_value=0.0)
-    BMI_prediction = ''
-    if st.button('Predict'):
-        prediction = BMI_model.predict([
-            [
-                gender_map[person_gender],
-                float(height),
-                float(weight)
-            ]
-        ])
-        if prediction[0] == 0:
-            BMI_prediction = 'Extremely Weak'
-        elif prediction[0] == 1:
-            BMI_prediction = 'Weak'
-        elif prediction[0] == 2:
-            BMI_prediction = 'Normal'
-        elif prediction[0] == 3:
-            BMI_prediction = 'Overweight'
-        elif prediction[0] == 4:
-            BMI_prediction = 'Obesity'
-        elif prediction[0] == 5:
-            BMI_prediction = 'Extreme Obesity'
-    st.success(BMI_prediction)
-if(selected == 'Loan'):
-    st.title('Loan Classification')
-    person_age = st.text_input('person_age')
-    person_gender = st.selectbox('person_gender', gender_map)
-    person_education = st.selectbox('person_education', education_map)
-    person_income = st.text_input('person_income') 
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.selectbox('person_home_ownership', home_map)
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.selectbox('loan_intent', intent_map)
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.selectbox(
-        'previous_loan_defaults_on_file',
-        default_map)
-    loan_prediction = ''
-    if st.button('Predict'):
-        loan_prediction = BMI_model.predict([
-            [
-                float(person_age),
-                gender_map[person_gender],
-                education_map[person_education],
-                float(person_income),
-                float(person_emp_exp),
-                home_map[person_home_ownership],
-                float(loan_amnt),
-                intent_map[loan_intent],
-                float(loan_int_rate),
-                float(loan_percent_income),
-                float(cb_person_cred_hist_length),
-                float(credit_score),
-                default_map[previous_loan_defaults_on_file]
-            ]
-        ])
-        if (loan_prediction[0] == 0):
-          loan_prediction = 'Not Accept'
-        else:
-          loan_prediction = 'Accept'
-    st.success(loan_prediction)
- 
- 
-if(selected == 'BMI'):
-    st.title('BMI Classification')
-    person_age = st.text_input('person_age')
-    person_gender = st.selectbox('person_gender', gender_map)
-    person_education = st.selectbox('person_education', education_map)
-    person_income = st.text_input('person_income') 
-    person_emp_exp = st.text_input('person_emp_exp')
-    person_home_ownership = st.selectbox('person_home_ownership', home_map)
-    loan_amnt = st.text_input('loan_amnt')
-    loan_intent = st.selectbox('loan_intent', intent_map)
-    loan_int_rate = st.text_input('loan_int_rate')
-    loan_percent_income = st.text_input('loan_percent_income')
-    cb_person_cred_hist_length = st.text_input('cb_person_cred_hist_length')
-    credit_score = st.text_input('credit_score')
-    previous_loan_defaults_on_file = st.selectbox(
-        'previous_loan_defaults_on_file',
-        default_map)
-    loan_prediction = ''
-    if st.button('Predict'):
-        loan_prediction = BMI_model.predict([
-            [
-                float(person_age),
-                gender_map[person_gender],
-                education_map[person_education],
-                float(person_income),
-                float(person_emp_exp),
-                home_map[person_home_ownership],
-                float(loan_amnt),
-                intent_map[loan_intent],
-                float(loan_int_rate),
-                float(loan_percent_income),
-                float(cb_person_cred_hist_length),
-                float(credit_score),
-                default_map[previous_loan_defaults_on_file]
-            ]
-        ])
-        if (loan_prediction[0] == 0):
-          loan_prediction = 'Not Accept'
-        else:
-          loan_prediction = 'Accept'
-    st.success(BMI_prediction)
- 
-if(selected == 'Riding'):
-    st.title('Riding Mower Classification')
-    Income = st.text_input('รายได้')
-    LotSize = st.text_input('พื้นที่บ้าน')
-    Riding_prediction = ''
-    if st.button('Predict'):
-        Riding_prediction = riding_model.predict([
-            [float(Income),float(LotSize)]
-        ])
-        if (Riding_prediction[0] == 0):
-          Riding_prediction = 'Non Owner'
-        else:
-          Riding_prediction = 'Owner'
+    person_gender = st.selectbox('Gender', ['Male', 'Female'])
+    person_height = st.number_input('Height (cm)', min_value=0, value=170)
+    person_weight = st.number_input('Weight (kg)', min_value=0, value=60)
 
  
-    st.success(Riding_prediction)
-
+    bmi_labels = {
+        0: 'Extremely Weak',
+        1: 'Weak',
+        2: 'Normal',
+        3: 'Overweight',
+        4: 'Obesity',
+        5: 'Extreme Obesity'
+    }
+    if st.button('Predict'):
+        gender_val = 0 if person_gender == 'Male' else 1
+        prediction = bmi_model.predict([[
+            gender_val, 
+            float(person_height), 
+            float(person_weight)
+        ]])
+        result_label = bmi_labels.get(int(prediction[0]), "Unknown")
+        st.success(f'ผลการทำนายคือ: **{result_label}**')
